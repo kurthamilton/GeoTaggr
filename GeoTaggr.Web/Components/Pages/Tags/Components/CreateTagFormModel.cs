0713fa6ac90783b7395be51bcc9db6a39a124424
Kurt Hamilton <kurt.j.hamilton@gmail.com>
2023-11-30 21:57:44 +0000

    Form improvements

---
+++ GeoTaggr.Web/Components/Pages/Tags/Components/CreateTagFormModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using GeoTaggr.Web.Components.Shared.Forms.Selects;
+
+namespace GeoTaggr.Web.Components.Pages.Tags.Components;
+
+public class CreateTagFormModel
+{
+    [Required]
+    public SelectOption? Country { get; set; }
+
+    [Required]
+    public string Name { get; set; } = "";
+
+    public string? Value { get; set; }
+
+    public double? Lat { get; set; }
+
+    public double? Long { get; set; }
+
+    public string? Url { get; set; }
+}
